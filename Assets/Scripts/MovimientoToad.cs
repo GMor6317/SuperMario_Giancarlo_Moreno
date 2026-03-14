ebecec530d36517df4fe368bf9f1b57b575b699e
GMor6317 <A01803172@tec.mx>
2026-03-13 20:37:59 -0600

    Añadiendo Goomba
    Se añade enemigo al juego

---
+++ Assets/Scripts/MovimientoToad.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class MovimientoToad : MonoBehaviour
+{
+    public float velocidadX = 2f;
+    public Rigidbody2D rb;
+    public Transform posicion;
+
+    private bool movimientoDerecha = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movimientoDerecha)
+        {
+            rb.linearVelocity = new Vector2(velocidadX, rb.linearVelocityY); 
+            if (posicion.position.x >= 7.7)
+            {
+                movimientoDerecha = false;
+            }   
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(-velocidadX, rb.linearVelocityY);
+            if (posicion.position.x <= -9.2)
+            {
+                movimientoDerecha = true;
+            }
+        }
+    }
+}
